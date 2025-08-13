5542c8b494a2cbb989ae4977c568d4febb55026c
dangvanmih <minhmeo2k413@gmail.com>
2025-08-13 23:40:34 +0700

    completed prjmn1

--- khóa Học Front-End/Bootstrap/project-mini-1/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-// js-tạo độ mượn của trang web khi scroll màn hình 
-document.addEventListener("DOMContentLoaded", function () {
-    const scroll = new LocomotiveScroll({
-        el: document.querySelector('[data-scroll-container]'),
-        smooth: true
-    });
-});
-// End-js-tạo độ mượn của trang web khi scroll màn hình
