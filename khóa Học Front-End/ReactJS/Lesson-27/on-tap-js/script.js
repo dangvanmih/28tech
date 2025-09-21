c2885a15baa74f0ef6632285535b8ff4331f2286
dangvanmih <minhmeo2k413@gmail.com>
2025-09-21 16:09:51 +0700

    bai-27

--- khóa Học Front-End/ReactJS/Lesson-27/on-tap-js/script.js
+++ khóa Học Front-End/ReactJS/Lesson-27/on-tap-js/script.js
@@ -1,5 +1,5 @@
 // Default Parameters (tham số mặc định)
-// const sum = (a = 0,b = 0) => { // gán a,b = o để khi người dùng ko truyền vào giá trị thì trả về 0 chứ ko phải NAN
+// const sum = (a = 0,b = 0) => { // gán a,b = 0 để khi người dùng ko truyền vào giá trị thì trả về 0 chứ ko phải NAN
 //     return a + b;
 // }
 //cách 2:
@@ -42,17 +42,43 @@
 // console.log(inforFinal);
 
 
-// Rest Parameter
+// Rest Parameter: tham số còn lại, là tham số đại diện cho những tham số không được khai báo của một function.
 
-const sum = (...numbers) => {
-    console.log(numbers);
+// Mảng numbers = [10,20,30,40,50,60].
+// reduce bắt đầu với total = 0
+// Lần 1: total = 0 + 10 = 10.
+// Lần 2: total = 10 + 20 = 30.
+// Lần 3: total = 30 + 30 = 60.
+// … tiếp tục cho đến hết.
 
-    const result = numbers.reduce((total, item) => {
-        return total + item;
-    },0);
+// const sum = (...numbers) => {
+//     console.log(numbers);
+
+//     const result = numbers.reduce((total, item) => {
+//         return total + item;
+//     },0);
 
 
-    return result;
-}
-console.log(sum(10,20,30,40,50,60));
-// 1:31
+//     return result;
+// }
+// console.log(sum(10,20,30,40,50,60));
+
+
+//Destructuring
+
+let infoUser = {
+    fullName: "Đặng Văn Minh",
+    email: "minh@gmail.com",
+ };
+
+ console.log(infoUser.fullName);
+ 
+
+ 
+ const {fullName , email} = infoUser;
+
+
+console.log(fullName);
+console.log(email);
+
+ 
---
+++ khóa Học Front-End/ReactJS/Lesson-28/hoc-bai-28/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  let name = "Đặng Văn Minh"
+  return (
+    <div className='test'>
+      Xin chào {name}!
+    </div>
+  );
+}
+
+export default App;
