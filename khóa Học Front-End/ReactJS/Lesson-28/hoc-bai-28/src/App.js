e8609908ec928f8f826fd7e6e0019f37f66eeb0b
dangvanmih <minhmeo2k413@gmail.com>
2025-09-21 16:45:06 +0700

    Lesson-28

--- khóa Học Front-End/ReactJS/Lesson-28/hoc-bai-28/src/App.js
+++ khóa Học Front-End/ReactJS/Lesson-28/hoc-bai-28/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import Header from './components/Header';
+
 
 function App() {
-  let name = "Đặng Văn Minh"
+  let name = "Đặng Văn Minh";
+  const css = {
+    color: "red",
+    backgroundColor: "blue"
+
+  }
+
+  // vì return chỉ trả ra 1 kết quả nên tất cả phải được bọc bởi 1 thẻ div,
+  // và nếu chỉ muốn hiển thị các Element bên trong ra thì bọc bởi thẻ Fragment <></>
   return (
-    <div className='test'>
-      Xin chào {name}!
-    </div>
+    <>
+      <Header/>
+      <div className='test' style={css}>
+        Xin chào {name}!
+      </div>
+      <div className='test' style={css}>
+        Xin chào {name}!
+      </div>
+    </>
   );
 }
 
