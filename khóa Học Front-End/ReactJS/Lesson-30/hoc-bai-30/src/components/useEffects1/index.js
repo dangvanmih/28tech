e4b5a49a65b53a07155acee5dace9b2e2610dcbc
dangvanmih <minhmeo2k413@gmail.com>
2025-10-20 21:18:19 +0700

    UseEffect no dependency

--- khóa Học Front-End/ReactJS/Lesson-30/hoc-bai-30/src/components/useEffects1/index.js
+++ khóa Học Front-End/ReactJS/Lesson-30/hoc-bai-30/src/components/useEffects1/index.js
@@ -1,5 +1,9 @@
+//trường hợp 1 khi useEffect không có dependence là nó sẽ render ra giao diện thì hàm callback sẽ được gọi lại kể cả render lần thứ 2 thì hàm callback vẫn được gọi lại
 import { useEffect } from "react";
 function UseEffect1() {
+    // truyền vào 1 hàm callback : hàm callback là 1 hàm được truyền vào đối số của 1 hàm khác
+    // bởi vì sau từ khóa return thì sẽ ko chạy được gì nên ta phải dùng useEffect
+    // để thực hiện 1 logic nào đó sau khi giao diện được render ra như dưới đây là lấy ra 3 thẻ li
     useEffect(() => {
         let listItem = document.querySelectorAll("ul li");
         console.log(listItem);
