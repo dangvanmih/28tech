2fec00a6e072625b923c0930e9b171cce4029d38
dangvanmih <minhmeo2k413@gmail.com>
2026-01-15 09:47:17 +0700

    ant-design

--- khóa Học Front-End/Ant_Design/learn-antd/src/App.js
+++ khóa Học Front-End/Ant_Design/learn-antd/src/App.js
@@ -1,28 +1,15 @@
-import { useState } from 'react';
 import './App.css';
-import {Button} from "antd";
-import {GithubOutlined} from "@ant-design/icons"
+// import LearnButton from './components/LearnButton/button';
+import LearnGrid from './components/learnGrid';
+
 function App() {
-  const [loading, setLoading] = useState(false)
-  const handleClick = () => {
-    setLoading(true)
-    setTimeout(() => {
-      const results = {
-        code:200,
-        data: []
-      }
-      if(results && results.code === 200) {
-        setLoading(false)
-      }
-    }, 3000);
-  }
+  
   return (
     <>
-      <Button type='primary' loading={loading} onClick={handleClick} disabled={loading} size='large'>Nội dung</Button>
-      <Button icon={<GithubOutlined  rotate={45} spin={true} />}>GitHub</Button>
+      {/* <LearnButton/> */}
+      <LearnGrid/>
     </>
   );
 }
 
 export default App;
-// 1:16
